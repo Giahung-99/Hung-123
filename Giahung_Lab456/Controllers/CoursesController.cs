d8acf2d750968ab6b930eb50b5df6cfa45b3d6fb
Administrator <Administrator@PC248>
2020-06-12 13:29:15 +0700

    Add project files.

---
+++ Giahung_Lab456/Controllers/CoursesController.cs
@@ -0,0 +1,46 @@
+﻿using Giahung_Lab456.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Giahung_Lab456.ViewModels;
+using Microsoft.AspNet.Identity;
+
+namespace Giahung_Lab456.Controllers
+{
+    public class CoursesController : Controller
+    {
+
+        private readonly ApplicationDbContext _dbConText;
+        public CoursesController()
+        {
+            _dbConText = new ApplicationDbContext();
+        }
+
+        // GET: Courses
+    
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbConText.Catagories.ToList()
+            };
+            return View(viewModel);
+
+        }
+
+    }
+    [Authorize]
+    [HttpPost]
+    public ActionResult Create(CourseViewModel ViewModel)
+    {
+        var course = new Course
+        {
+            LecturerId = User.Identity.GetUserId(),
+            DateTime = ViewModel.GetDateTime(),
+            CategoryId = ViewModel.Category,
+            Place = ViewModel.Place
+        };
+    }
+}
---
+++ Giahung_Lab456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Giahung_Lab456.MvcApplication" Language="C#" %>
